189d9ad46e602a5d9a099f7e17285c3cea21d478
Sean Omlor <somlor@eml.cc>
2017-04-04 11:08:11 -0700

    world trotter add segmented control to map

--- WorldTrotter/WorldTrotter/MapViewController.swift
+++ WorldTrotter/WorldTrotter/MapViewController.swift
@@ -13,8 +13,31 @@ class MapViewController: UIViewController {
   var mapView: MKMapView!
 
   override func loadView() {
+    // add map
     mapView = MKMapView()
     view = mapView
+
+    // add segmented control
+    let segmentedControl = UISegmentedControl(items: [
+      "Standard",
+      "Hybrid",
+      "Satellite",
+    ])
+    segmentedControl.selectedSegmentIndex = 0
+    segmentedControl.translatesAutoresizingMaskIntoConstraints = false
+    segmentedControl.backgroundColor = UIColor.white.withAlphaComponent(0.5)
+    view.addSubview(segmentedControl)
+
+    // add control constraints
+    let topConstraint =
+      segmentedControl.topAnchor.constraint(equalTo: view.topAnchor)
+    let leadingConstraint =
+      segmentedControl.leadingAnchor.constraint(equalTo: view.leadingAnchor)
+    let trailingConstraint =
+      segmentedControl.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+    topConstraint.isActive = true
+    leadingConstraint.isActive = true
+    trailingConstraint.isActive = true
   }
 
   override func viewDidLoad() {
