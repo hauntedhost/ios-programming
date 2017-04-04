f742499618de6e44279fb0a2e6246b1a041b46a2
Sean Omlor <somlor@eml.cc>
2017-04-04 10:03:53 -0700

    commit unfinished protocols exercise

---
+++ Protocols/Protocols/Person.swift
@@ -0,0 +1,13 @@
+//
+//  Person.swift
+//  Protocols
+//
+//  Created by sean on 4/2/17.
+//  Copyright © 2017 sean. All rights reserved.
+//
+
+import Foundation
+
+struct Person {
+  
+}
---
+++ Protocols/Protocols/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  Protocols
+//
+//  Created by sean on 4/2/17.
+//  Copyright © 2017 sean. All rights reserved.
+//
+
+import Foundation
+
+
