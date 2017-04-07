17b0772aa60b09328c90dc912ce7480ffff8a58f
Sean Omlor <somlor@eml.cc>
2017-04-06 20:34:27 -0700

    an interesting photo appears

--- Photorama/Photorama/PhotoStore.swift
+++ Photorama/Photorama/PhotoStore.swift
@@ -6,7 +6,16 @@
 //  Copyright © 2017 sean. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+enum ImageResult {
+  case success(UIImage)
+  case failure(Error)
+}
+
+enum PhotoError: Error {
+  case imageCreationError
+}
 
 enum PhotosResult {
   case success([Photo])
@@ -31,6 +40,33 @@ class PhotoStore {
       completion(result)
     }
     task.resume()
+  }
+
+  public func fetchImage(
+    for photo: Photo,
+    completion: @escaping (ImageResult) -> Void
+  ) {
+    let photoURL = photo.remoteURL
+    let request = URLRequest(url: photoURL)
+    let task = session.dataTask(with: request) { (data, resp, err) -> Void in
+      let result = self.processImageRequest(data: data, error: err)
+      completion(result)
+    }
+    task.resume()
+  }
+
+  private func processImageRequest(data: Data?, error: Error?) -> ImageResult {
+    guard
+      let imageData = data,
+      let image = UIImage(data: imageData)
+    else {
+      if data == nil {
+        return .failure(error!)
+      } else {
+        return .failure(PhotoError.imageCreationError)
+      }
+    }
+    return .success(image)
   }
 
   private func processPhotosRequest(data: Data?, error: Error?) -> PhotosResult {
